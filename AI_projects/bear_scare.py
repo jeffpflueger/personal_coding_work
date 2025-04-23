ecef3d3ceadae5672404fb41474570d7b4e6af4d
Jeff Pflueger <photo@jeffpflueger.com>
2025-04-23 13:41:17 -0700

    Update bear_scare.py
    complete bear scare script with the codec issue resolved

--- AI_projects/bear_scare.py
+++ AI_projects/bear_scare.py
@@ -1,19 +1,23 @@
-# Imports
+# Bear Scare Tensorflow-trained Classifier and OpenCV with Video Recording #
+
 import os
+import argparse
 import cv2
 import numpy as np
+import sys
 import time
 from threading import Thread
-import shutil
-from datetime import datetime
+import importlib.util
 import RPi.GPIO as GPIO
+from datetime import datetime
 
-# Constants
+# Video settings
 VIDEO_DURATION = 30  # seconds
-VIDEO_DIR = "bear_videos"
-MAX_VIDEOS = 100
+VIDEO_DIR = os.path.join(os.getcwd(), "bear_videos")
+MAX_VIDEO_FILES = 100
+os.makedirs(VIDEO_DIR, exist_ok=True)
 
-# Set up GPIO
+# GPIO setup
 led = 40
 led2 = 11
 led_count = 11
@@ -22,11 +26,7 @@ GPIO.setwarnings(False)
 GPIO.setup(led, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 
-# Create folder if it doesn't exist
-if not os.path.exists(VIDEO_DIR):
-    os.makedirs(VIDEO_DIR)
-
-# VideoStream class (unchanged)
+# VideoStream class
 class VideoStream:
     def __init__(self, resolution=(640, 480), framerate=30):
         self.stream = cv2.VideoCapture(0)
@@ -41,7 +41,10 @@ class VideoStream:
         return self
 
     def update(self):
-        while not self.stopped:
+        while True:
+            if self.stopped:
+                self.stream.release()
+                return
             (self.grabbed, self.frame) = self.stream.read()
 
     def read(self):
@@ -49,73 +52,167 @@ class VideoStream:
 
     def stop(self):
         self.stopped = True
-        self.stream.release()
 
-# Function to delete oldest videos if over limit
+# Cleanup old videos
 def cleanup_old_videos():
-    files = sorted([os.path.join(VIDEO_DIR, f) for f in os.listdir(VIDEO_DIR)],
-                   key=os.path.getctime)
-    while len(files) > MAX_VIDEOS:
-        os.remove(files[0])
-        files.pop(0)
+    video_files = sorted(
+        [os.path.join(VIDEO_DIR, f) for f in os.listdir(VIDEO_DIR) if f.endswith('.avi')],
+        key=os.path.getctime
+    )
+    while len(video_files) > MAX_VIDEO_FILES:
+        os.remove(video_files[0])
+        video_files.pop(0)
 
-# Function to record video
+# Record video
 def record_bear_video(videostream, fps=30):
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     filename = os.path.join(VIDEO_DIR, f'bear_{timestamp}.avi')
-    frame_width = int(videostream.stream.get(3))
-    frame_height = int(videostream.stream.get(4))
+    frame_width = int(videostream.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(videostream.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'MJPG'), fps, (frame_width, frame_height))
 
-    out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'XVID'), fps, (frame_width, frame_height))
     start_time = time.time()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.7
+    font_color = (0, 0, 255)
+    thickness = 2
+    line_type = cv2.LINE_AA
+
     while time.time() - start_time < VIDEO_DURATION:
         frame = videostream.read()
-        out.write(frame)
-        time.sleep(1 / fps)
+        if frame is not None:
+            now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cv2.putText(frame, "Bear Scare Horn Activated", (10, 30), font, font_scale, font_color, thickness, line_type)
+            cv2.putText(frame, now, (10, 60), font, font_scale, font_color, thickness, line_type)
+            out.write(frame)
     out.release()
-    print(f"[INFO] Video saved: {filename}")
     cleanup_old_videos()
 
-# TensorFlow Lite & OpenCV setup (same as before)
-# ...
+# Argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument('--modeldir', required=True)
+parser.add_argument('--graph', default='detect.tflite')
+parser.add_argument('--labels', default='labelmap.txt')
+parser.add_argument('--threshold', default=0.65)
+parser.add_argument('--resolution', default='800x480')
+parser.add_argument('--edgetpu', action='store_true')
+args = parser.parse_args()
 
-# Initialize videostream
-videostream = VideoStream(resolution=(800, 480), framerate=30).start()
+MODEL_NAME = args.modeldir
+GRAPH_NAME = args.graph
+LABELMAP_NAME = args.labels
+min_conf_threshold = float(args.threshold)
+resW, resH = args.resolution.split('x')
+imW, imH = int(resW), int(resH)
+use_TPU = args.edgetpu
+
+pkg = importlib.util.find_spec('tflite_runtime')
+if pkg:
+    from tflite_runtime.interpreter import Interpreter
+    if use_TPU:
+        from tflite_runtime.interpreter import load_delegate
+else:
+    from tensorflow.lite.python.interpreter import Interpreter
+    if use_TPU:
+        from tensorflow.lite.python.interpreter import load_delegate
+
+if use_TPU:
+    if (GRAPH_NAME == 'detect.tflite'):
+        GRAPH_NAME = 'edgetpu.tflite'
+
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
+PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
+
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+if labels[0] == '???':
+    del(labels[0])
+
+if use_TPU:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT,
+                              experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+else:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT)
+
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+floating_model = (input_details[0]['dtype'] == np.float32)
+input_mean = 127.5
+input_std = 127.5
+
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
+videostream = VideoStream(resolution=(imW, imH), framerate=30).start()
 time.sleep(1)
 
-# Main loop
 while True:
     t1 = cv2.getTickCount()
-    frame = videostream.read()
-    # Preprocess frame and run detection as before...
-    # Set `object_name = labels[int(classes[i])]` in loop
+    frame1 = videostream.read()
+    frame = frame1.copy()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
+    input_data = np.expand_dims(frame_resized, axis=0)
+
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0]
+    classes = interpreter.get_tensor(output_details[1]['index'])[0]
+    scores = interpreter.get_tensor(output_details[2]['index'])[0]
 
     for i in range(len(scores)):
-        if scores[i] > min_conf_threshold:
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+            ymin = int(max(1, (boxes[i][0] * imH)))
+            xmin = int(max(1, (boxes[i][1] * imW)))
+            ymax = int(min(imH, (boxes[i][2] * imH)))
+            xmax = int(min(imW, (boxes[i][3] * imW)))
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+
             object_name = labels[int(classes[i])]
-            if object_name == "bear" and int(scores[i] * 100) > 55:
-                print("[ALERT] BEAR DETECTED!")
+            label = '%s: %d%%' % (object_name, int(scores[i]*100))
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+            label_ymin = max(ymin, labelSize[1] + 10)
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10),
+                          (xmin+labelSize[0], label_ymin+baseLine-10),
+                          (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, label, (xmin, label_ymin-7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+
+            if object_name == "bear" and int(scores[i]*100) > 55:
+                print("BEAR!!!! - Score:" + str(int(scores[i]*100)))
                 GPIO.output(led, GPIO.HIGH)
                 GPIO.output(led2, GPIO.HIGH)
                 led_count = 0
                 record_bear_video(videostream)
 
-    # LED Blinking logic (same as before)
     led_count += 1
     if led_count > 10:
         GPIO.output(led, GPIO.LOW)
         GPIO.output(led2, GPIO.LOW)
-    elif led_count % 2 == 0:
+    elif (led_count % 2) == 0:
         GPIO.output(led, GPIO.LOW)
         GPIO.output(led2, GPIO.LOW)
     else:
         GPIO.output(led, GPIO.HIGH)
         GPIO.output(led2, GPIO.HIGH)
 
+    cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+
     cv2.imshow('Object detector', frame)
+    t2 = cv2.getTickCount()
+    time1 = (t2 - t1) / freq
+    frame_rate_calc = 1 / time1
+
     if cv2.waitKey(1) == ord('q'):
         break
 
-# Cleanup
 cv2.destroyAllWindows()
 videostream.stop()
